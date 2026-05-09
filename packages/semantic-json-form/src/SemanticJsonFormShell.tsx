982c5839a097a245a33b4222bf7748f2f3b41a70
Winzlieb <winzlieb@c3d2.de>
2026-05-09 16:26:52 +0200

    chore: better naming SemanticJsonFormShell component to the semantic-json-form package
    - Introduced a new SemanticJsonFormShell component that exports the existing SemanticJsonFormNoOps for better naming.
    - Updated index.ts to include the new component in the exports.

---
+++ packages/semantic-json-form/src/SemanticJsonFormShell.tsx
@@ -0,0 +1,1 @@
+export { SemanticJsonFormNoOps as SemanticJsonFormShell } from "./SemanticJsonFormNoOps";
--- packages/semantic-json-form/src/index.ts
+++ packages/semantic-json-form/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./SemanticJsonForm";
 export * from "./SemanticJsonFormToolbar";
+export * from "./SemanticJsonFormShell";
 export * from "./SemanticJsonFormNoOps";
 export * from "./GenericForm";
 export * from "./helper";
